000c9cc55190cf769ee9fa620433576f5c9a894b
RaunG <raung@icloud.com>
2018-10-19 23:05:02 -0500

    Update blog_project/mysite/blog/views.py

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
+from django.utils import timezone
 from blog.models import Post,Comment
 from blog.forms import PostForm,CommentForm
 from django.urls import reverse_lazy
@@ -44,3 +45,27 @@ class DraftListView(LoginRequiredMixin,ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date_isnull=True).order_by('created_date')
+
+
+######################################
+######################################
+
+@login_required
+def add_comment_to_post(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    if request.method == 'Post':
+        form = CommentForm(request.Post)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail',pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request,'blog/comment_form.html',{'form':form})
+
+@login_required
+def comment_approve(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    comment.approve()
+    return redirect('post_detail',pk=comment.post.pk)
