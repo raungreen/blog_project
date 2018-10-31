416d3be9c9a3d555bfd85e803bb012290dae95c1
RaunG <raung@icloud.com>
2018-10-31 14:17:00 -0500

    Update blog_project/mysite/blog/views.py

--- blog_project/mysite/blog/views.py
+++ blog_project/mysite/blog/views.py
@@ -51,6 +51,13 @@ class DraftListView(LoginRequiredMixin,ListView):
 ######################################
 
 @login_required
+def post_publish(request,pk):
+    post = get_object_or_404(Post,pk=pk)
+    post.publish
+    return redirect('post_detail',pk=pk)
+
+
+@login_required
 def add_comment_to_post(request,pk):
     post = get_object_or_404(Post,pk=pk)
     if request.method == 'Post':
@@ -69,3 +76,10 @@ def comment_approve(request,pk):
     comment = get_object_or_404(Comment,pk=pk)
     comment.approve()
     return redirect('post_detail',pk=comment.post.pk)
+
+@login_required
+def comment_remove(request,pk):
+    comment = get_object_or_404(Comment,pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail',pk=post_pk)
